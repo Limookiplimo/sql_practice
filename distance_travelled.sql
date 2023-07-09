39f9ad60cc066fd4fcb311ebcc396a6bbdefbbdd
Limoo Kiplimo <limoodrice@gmail.com>
2023-07-09 21:31:44 +0300

    updated code structure

--- distance_travelled.sql
+++ distance_travelled.sql
@@ -1,7 +1,6 @@
 /*
-Find the top 10 users that have traveled the greatest distance. Output their id, name and a total distance traveled.
-
-Tables: lyft_rides_log, lyft_users
+Difficulty - Medium
+source: https://platform.stratascratch.com/coding/10324-distances-traveled?code_type=1
 */
 
 -- Thought Process --
@@ -15,13 +14,9 @@ select
     user_id,
     name,
     sum(distance) as total_distance
-from 
-    lyft_rides_log r
+from lyft_rides_log r
 inner join 
     lyft_users u on r.user_id = u.id
-group by 
-    user_id,
-    name
-order by 
-    total_distance desc
+group by user_id, name
+order by total_distance desc
 limit 10;
